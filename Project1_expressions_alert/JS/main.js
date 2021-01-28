5f2cdee090d947e6a7ddfca3d7726fa8032c62b4
Joe <josephomalley135@gmail.com>
2021-01-28 07:12:59 -0600

    add js

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var one = "A", two = "B", three = "C";  //Defining a var
+var one = one.fontcolor("red");         //defining var one's color 
+var two = two.fontcolor("blue");        //defining var two's color
+var three = three.fontcolor("green");   //defining var three's color
+document.write(3+3);                    //executing an expression
+window.alert("window alert example")    //basic window alert example
+document.write(one + two)               //varible concatenation
