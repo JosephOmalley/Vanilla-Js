a2a74ff8739c18f71187ad5164a2da40ff82a645
Joe <josephomalley135@gmail.com>
2021-01-29 20:54:20 -0600

    Project5

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,15 @@
+document.write(typeof 1.2 )
+
+document.write(2 == 2 )
+
+document.write(2 === '2' )
+
+document.write(2 > 2 )
+
+document.write(2 < 20)
+
+document.write(2 > 2 || 3 > 2 )
+
+document.write(2 > 2 && 3 > 2 )
+
+document.write(!(2 > 2 && 3 > 2))
