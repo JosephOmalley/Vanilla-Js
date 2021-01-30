dbeb5de27d1b13c016ab63662221dcdb3ec9c887
Joe <josephomalley135@gmail.com>
2021-01-29 20:57:42 -0600

    add comment

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -1,15 +1,15 @@
-document.write(typeof 1.2 )
+document.write(typeof 1.2 )                             //display typeof 
 
-document.write(2 == 2 )
+document.write(2 == 2 )                                 //display check equal
 
-document.write(2 === '2' )
+document.write(2 === '2' )                              //display check data type
 
-document.write(2 > 2 )
+document.write(2 > 2 )                                  //display check less then
 
-document.write(2 < 20)
+document.write(2 < 20)                                  //display check greater then
 
-document.write(2 > 2 || 3 > 2 )
+document.write(2 > 2 || 3 > 2 )                         //display check or
 
-document.write(2 > 2 && 3 > 2 )
+document.write(2 > 2 && 3 > 2 )                         //dispaly check or
 
-document.write(!(2 > 2 && 3 > 2))
+document.write(!(2 > 2 && 3 > 2))                       //display check not
