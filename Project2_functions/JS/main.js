a12c33f69a24e9b733556aface3e5b6fa97b345d
Joe <josephomalley135@gmail.com>
2021-02-16 16:28:09 -0600

    update

--- Project2_functions/JS/main.js
+++
@@ -1,7 +0,0 @@
-function grey_Button() {                                        //def function
-    var str = "can you see me\,";                               //define str
-    str += " even more now that I concatenated the string\?";    //add to str
-    var result = str.fontcolor("grey");                         //makes font color grey
-
-    document.getElementById("greyButton").innerHTML = result;   //declare id and result
-}
--- Project8_string_methods/JS/main.js
+++
@@ -1,8 +0,0 @@
-function pro8_function() {                                                             //create function "pro8_function"
-    var num0  = 123.5 ;                                                                //define number  
-    var str0 = " this number "                                                         //define str
-    var str1 = " rounded up is =  out"                                                 //define str             
-    var num1 = 123.5                                                                   //define str     
-    var result = str0.concat(num0.toString(), str1.slice(0, 17), num1.toPrecision(3))  //concat 4 var, make num0 to str, display between 0 and 17 in str1, display num1 toPrecision(3)  
-    document.getElementById("Pro8").innerHTML = result;
-}
